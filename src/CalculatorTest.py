e7c72c00039ffea723d02b399827dce61bfdd45f
Janki01 <jjk53@njit.edu>
2019-11-28 14:43:53 -0500

    Task : Added Addition function

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -1,11 +1,22 @@
 import unittest
+from CsvReader import CsvReader
 from Calculator import Calculator
 
 
 class MyTestCase(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
+
+    def test_addition(self):
+        test_data = CsvReader('src/Addition.csv').data
+        for row in test_data:
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), result)
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
 
 if __name__ == '__main__':
